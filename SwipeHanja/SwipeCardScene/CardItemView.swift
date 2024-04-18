fd336c20836442ff593cc526ca552d81319dadb8
WGYang-KR <anto.wg.yang@gmail.com>
2024-04-19 01:31:35 +0900

    Koloda 카드 라이브러리 테스트중(Overlay뷰 커스터마이징, 탭 이벤트 구현 필요)

---
+++ SwipeHanja/SwipeCardScene/CardItemView.swift
@@ -0,0 +1,38 @@
+//
+//  CardItemView.swift
+//  SwipeHanja
+//
+//  Created by Anto-Yang on 4/19/24.
+//
+
+import UIKit
+
+class CardItemView: UIView {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    convenience init(text: String) {
+        self.init(frame: .zero)
+        label.text = text
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        customInit()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        customInit()
+
+    }
+
+    func customInit() {
+        if let view = Bundle.main.loadNibNamed("\(CardItemView.self)", owner: self, options: nil)?.first as? UIView {
+            view.frame = self.bounds
+            addSubview(view)
+        }
+    }
+
+}
--- SwipeHanja/SwipeCardScene/SwipeCardVC.swift
+++ SwipeHanja/SwipeCardScene/SwipeCardVC.swift
@@ -6,22 +6,86 @@
 //
 
 import UIKit
+import Koloda
+
+let sampleData: [CardItem] = [CardItem(id: 0, frontWord: "One", backWord: "일"),
+                              CardItem(id: 1, frontWord: "Two", backWord: "이"),
+                              CardItem(id: 2, frontWord: "Three", backWord: "삼"),
+                              CardItem(id: 3, frontWord: "Four", backWord: "사"),
+                              CardItem(id: 4, frontWord: "Five", backWord: "오")]
 
 class SwipeCardVC: UIViewController {
 
-    var cardList: [CardItem] = [CardItem(id: 0, frontWord: "One", backWord: "일"),
-                               CardItem(id: 1, frontWord: "Two", backWord: "이"),
-                               CardItem(id: 2, frontWord: "Three", backWord: "삼"),
-                               CardItem(id: 3, frontWord: "Four", backWord: "사"),
-                               CardItem(id: 4, frontWord: "Five", backWord: "오")]
+    
+    var dataSource: [CardItem] = sampleData
+    
+    @IBOutlet weak var kolodaView: KolodaView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        kolodaView.dataSource = self
+        kolodaView.delegate = self
+        
+        self.modalTransitionStyle = UIModalTransitionStyle.flipHorizontal
+    }
+    // MARK: IBActions
 
+    @IBAction func leftButtonTapped() {
+        kolodaView?.swipe(.left)
     }
     
-    @IBAction func didTapBackBtn(_ sender: Any) {
+    @IBAction func rightButtonTapped() {
+        kolodaView?.swipe(.right)
+    }
+    
+    @IBAction func undoButtonTapped() {
+        kolodaView?.revertAction()
+    }
+    
+    @IBAction func topBackBtnTapped(_ sender: Any) {
         self.moveBackVC(animated: true)
     }
     
 }
+
+// MARK: KolodaViewDelegate
+
+extension SwipeCardVC: KolodaViewDelegate {
+    
+    func kolodaDidRunOutOfCards(_ koloda: KolodaView) {
+        let position = kolodaView.currentCardIndex
+        let increasingNumber = sampleData.count
+        dataSource += sampleData
+
+        kolodaView.insertCardAtIndexRange(position..<position + increasingNumber, animated: true)
+    }
+    
+    func koloda(_ koloda: KolodaView, didSelectCardAt index: Int) {
+//        UIApplication.shared.openURL(URL(string: "https://yalantis.com/")!)
+    }
+
+}
+
+// MARK: KolodaViewDataSource
+
+extension SwipeCardVC: KolodaViewDataSource {
+    
+    func kolodaNumberOfCards(_ koloda: KolodaView) -> Int {
+        return dataSource.count
+    }
+    
+    func kolodaSpeedThatCardShouldDrag(_ koloda: KolodaView) -> DragSpeed {
+        return .default
+    }
+    
+    func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
+        return  CardItemView(text: dataSource[index].frontWord)
+    }
+    
+    func koloda(_ koloda: KolodaView, viewForCardOverlayAt index: Int) -> OverlayView? {
+        return Bundle.main.loadNibNamed("OverlayView", owner: self, options: nil)?[0] as? OverlayView
+    }
+}
+
