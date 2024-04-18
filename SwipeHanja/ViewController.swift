a87f0a106cd36b70580c0661de8b2714a5b148cc
WGYang-KR <anto.wg.yang@gmail.com>
2024-04-18 21:48:04 +0900

    Initial Commit

---
+++ SwipeHanja/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwipeHanja
+//
+//  Created by Anto-Yang on 4/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
