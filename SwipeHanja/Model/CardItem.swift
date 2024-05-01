09a78a7f1de810e529cda9b184bd675dfc33ea8d
WGYang-KR <anto.wg.yang@gmail.com>
2024-05-01 21:06:12 +0900

    값 변경 옵저버 추가

--- SwipeHanja/Model/CardItem.swift
+++ SwipeHanja/Model/CardItem.swift
@@ -8,7 +8,7 @@
 import Foundation
 import RealmSwift
 
-final class CardItem: Object, Codable {
+final class CardItem: Object, Decodable {
    
    
     @Persisted(primaryKey: true) var _id: ObjectId
@@ -19,19 +19,29 @@ final class CardItem: Object, Codable {
     @Persisted var hasShown: Bool
     @Persisted var hasMemorized: Bool
 
-    //샘플데이터용
-    internal init(_id: ObjectId, index: Int, level: Int, frontWord: String, backWord: String, hasShown: Bool, hasMemorized: Bool) {
+    // Realm에 자동으로 업데이트되도록 변수를 관찰
+    private var notificationToken: NotificationToken?
+    
+    override init() {
         super.init()
-        self._id = _id
-        self.index = index
-        self.level = level
-        self.frontWord = frontWord
-        self.backWord = backWord
-        self.hasShown = hasShown
-        self.hasMemorized = hasMemorized
+        setObserver()
     }
     
-    //Decodable
+    deinit {
+        removeObserver()
+    }
+    
+    //MARK: - Decodable
+    enum CodingKeys: String, CodingKey {
+        case index
+        case level
+        case frontWord
+        case backWord
+        case hasShown
+        case hasMemorized
+        
+    }
+    
     init(from decoder: Decoder) throws {
         super.init()
         let container = try decoder.container(keyedBy: CodingKeys.self)
@@ -45,4 +55,46 @@ final class CardItem: Object, Codable {
     
     }
     
+    //MARK: -
+    func setObserver() {
+        // Realm에 추가되었을 때의 변화를 감지하는 코드
+        notificationToken = self.observe { (change) in
+            switch change {
+            case .change:
+                // 변화가 감지되면 Realm에 자동으로 저장
+                do {
+                    let realm = try Realm()
+                    try realm.write {
+                        realm.add(self, update: .modified)
+                    }
+                } catch {
+                    shLog("An error occurred: \(error)")
+                }
+            case .error(let error):
+                // 에러 처리
+                shLog("An error occurred: \(error)")
+            case .deleted:
+                // 객체가 삭제되면 추가 작업 수행
+                shLog("Object deleted")
+            }
+        }
+    }
+    
+    func removeObserver() {
+        // 옵저버가 더 이상 필요하지 않을 때 메모리 해제
+        notificationToken?.invalidate()
+    }
+    
+    //MARK: -
+    //샘플데이터용
+    internal init(_id: ObjectId, index: Int, level: Int, frontWord: String, backWord: String, hasShown: Bool, hasMemorized: Bool) {
+        super.init()
+        self._id = _id
+        self.index = index
+        self.level = level
+        self.frontWord = frontWord
+        self.backWord = backWord
+        self.hasShown = hasShown
+        self.hasMemorized = hasMemorized
+    }
 }
--- SwipeHanja/Model/CardPack.swift
+++ SwipeHanja/Model/CardPack.swift
@@ -8,8 +8,7 @@
 import Foundation
 import RealmSwift
 
-final class CardPack: Object, Codable{
-   
+final class CardPack: Object, Decodable{
     
     @Persisted(primaryKey: true) var _id: ObjectId
     @Persisted var index: Int
@@ -17,18 +16,27 @@ final class CardPack: Object, Codable{
     @Persisted var title: String //한자 9급
     @Persisted var cardList: List<CardItem> // = List<CardItem>()
     
+    // Realm에 자동으로 업데이트되도록 변수를 관찰
+    private var notificationToken: NotificationToken?
     
-    //샘플데이터용
-    internal init(_id: ObjectId, index: Int, level: Int, title: String, cardList: List<CardItem>) {
+    override init() {
         super.init()
-        self._id = _id
-        self.index = index
-        self.level = level
-        self.title = title
-        self.cardList = cardList
+        setObserver()
     }
     
-    //Decodable
+    deinit {
+        removeObserver()
+    }
+    
+    
+    //MARK: - Decodable
+    enum CodingKeys: String, CodingKey {
+        case index
+        case level
+        case title
+        case cardList
+    }
+    
     init(from decoder: Decoder) throws {
         super.init()
         
@@ -38,11 +46,55 @@ final class CardPack: Object, Codable{
         level = try container.decodeIfPresent(Int.self, forKey: .level) ?? 0
         title = try container.decodeIfPresent(String.self, forKey: .title) ?? ""
         let cardArray = try container.decodeIfPresent([CardItem].self, forKey: .cardList) ?? []
-        var newCardList = List<CardItem>()
+        let newCardList = List<CardItem>()
         for item in cardArray {
             newCardList.append(item)
         }
+        
+        notificationToken = nil
+        
+    }
     
+    //MARK: -
+    func setObserver() {
+        // Realm에 추가되었을 때의 변화를 감지하는 코드
+        notificationToken = self.observe { (change) in
+            switch change {
+            case .change:
+                // 변화가 감지되면 Realm에 자동으로 저장
+                do {
+                    let realm = try Realm()
+                    try realm.write {
+                        realm.add(self, update: .modified)
+                    }
+                } catch {
+                    shLog("An error occurred: \(error)")
+                }
+            case .error(let error):
+                // 에러 처리
+                shLog("An error occurred: \(error)")
+            case .deleted:
+                // 객체가 삭제되면 추가 작업 수행
+                shLog("Object deleted")
+            }
+        }
+    }
+    
+    func removeObserver() {
+        // 옵저버가 더 이상 필요하지 않을 때 메모리 해제
+        notificationToken?.invalidate()
+    }
+    
+    //MARK: - 
+    //샘플데이터용
+    internal init(_id: ObjectId, index: Int, level: Int, title: String, cardList: List<CardItem>) {
+        super.init()
+        self._id = _id
+        self.index = index
+        self.level = level
+        self.title = title
+        self.cardList = cardList
+        
     }
 }
 
