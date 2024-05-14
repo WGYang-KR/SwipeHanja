2595220c404ea424d215328d34a3b4d31d274ad9
WGYang-KR <anto.wg.yang@gmail.com>
2024-05-14 17:39:47 +0900

    완료팝업 구현중

---
+++ SwipeHanja/SwipeCardScene/SwipeCardCompletionPopUpVC.swift
@@ -0,0 +1,36 @@
+//
+//  SwipeCardCompletionPopUpVC.swift
+//  SwipeHanja
+//
+//  Created by WG-Yang on 5/14/24.
+//
+
+import UIKit
+
+class SwipeCardCompletionPopUpVC: UIViewController {
+
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var bottomButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
