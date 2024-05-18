e7da6c8340021a779f572c4e469e40c47e5ccab7
WGYang-KR <anto.wg.yang@gmail.com>
2024-05-18 20:24:12 +0900

    구글 애널리틱스 크래쉬리틱스 설치

--- SwipeHanja/AppDelegate.swift
+++ SwipeHanja/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Use the Firebase library to configure APIs.
+        FirebaseApp.configure()
+        
+        let title = "AppDidFinishLaunchingWithOptions"
+        Analytics.logEvent(AnalyticsEventSelectContent, parameters: [
+          AnalyticsParameterItemID: "id-\(title)",
+          AnalyticsParameterItemName: "\(title)",
+          AnalyticsParameterContentType: "cont",
+        ])
+        
         return true
     }
 
