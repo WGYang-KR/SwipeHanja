beecc3fa7bf5c9cbd32ed4862221b3cf4d35baad
WGYang-KR <anto.wg.yang@gmail.com>
2024-12-31 00:37:36 +0900

    한자데이터 업데이트

--- SwipeHanja/MainScene/MainVM.swift
+++ SwipeHanja/MainScene/MainVM.swift
@@ -58,7 +58,7 @@ class MainVM {
     ///DB를 JSON에서 불러와서 저장한다.
     private func initCardPackDBFromJson() {
         do {
-            let cardPackList = try JSONSerialization.loadJSONFromFile(filename: "CardPacks", type: [CardPack].self)
+            let cardPackList = try JSONSerialization.loadJSONFromFile(filename: "CardPacks_20241231", type: [CardPack].self)
             let realm = try Realm()
             try realm.write {
                 realm.add(cardPackList)
