6ed5491f5d6e8ee352be9fe8236e923055769d06
WGYang-KR <anto.wg.yang@gmail.com>
2024-12-31 15:18:51 +0900

    DB 업데이트시에 ObjectId 삽입 삭제

--- SwipeHanja/Resource/DBManager.swift
+++ SwipeHanja/Resource/DBManager.swift
@@ -19,7 +19,7 @@ class DBManager {
       
         ///objectID 이미그레이션(업데이트) 필요여부
         var isUpdateObjectIdNeeded = false
-        
+    
         let config = Realm.Configuration(schemaVersion: 2, migrationBlock: { migration, oldSchemaVersion in
             shLog("oldSchemaVersion: \(oldSchemaVersion)")
             isUpdateObjectIdNeeded = true
@@ -68,11 +68,7 @@ class DBManager {
         // Use this configuration when opening realms
         Realm.Configuration.defaultConfiguration = config
         shLog("realm 위치: \(Realm.Configuration.defaultConfiguration.fileURL?.absoluteString ?? "")")
-        
-        //DB 속성 확장을 한 경우에 이미그레이션 진행
-        if isUpdateObjectIdNeeded{
-                updateObjectIDs()
-        }
+
     }
     
     ///전체 카드 리스트를, CardPack  JSON에서 불러온다
@@ -84,53 +80,6 @@ class DBManager {
         } catch(let error) {
             cardPacks = []
             totalCardItems = []
-            shLog(error.localizedDescription)
-        }
-    }
-    
-    ///기존 데이터 objectID 지정해서 다시 삽입
-    private func updateObjectIDs() {
-
-        let realm = try! Realm()
-        let oldObjects = realm.objects(CardItem.self) // 기존 객체 가져오기
-        shLog("ObjectID 삽입 시작: \(oldObjects.count)")
-        
-        var totalCount = 0
-        var successCount = 0
-        var errorCount = 0
-        
-        //새 데이터 추가
-        for cardItem in totalCardItems {
-            totalCount += 1
-            do {
-                try realm.write {
-                    if let oldObject = oldObjects.first(where: { $0.frontWord == cardItem.frontWord }) {
-                        cardItem.hasShown = oldObject.hasShown
-                        cardItem.hasMemorized = oldObject.hasMemorized
-                        cardItem.isFavorite = oldObject.isFavorite
-                        cardItem.favoriteData = oldObject.favoriteData
-                        // 새 객체 추가
-                        realm.add(cardItem)
-                        successCount += 1
-                    } else {
-                        errorCount += 1
-                    }
-                }
-            } catch(let error) {
-                errorCount += 1
-                shLog(error.localizedDescription)
-            }
-        }
-        
-        shLog("새 데이터 추가 완료: totalCount: \(totalCount), successCount: \(successCount), errorCount:\(errorCount) ")
-        
-        // 기존 데이터 삭제
-        do {
-            try realm.write {
-                realm.delete(oldObjects)
-            }
-            shLog("기존 데이터 삭제완료")
-        } catch {
             shLog(error.localizedDescription)
         }
     }
